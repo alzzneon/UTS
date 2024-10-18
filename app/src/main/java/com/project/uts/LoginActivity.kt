65a3fefff2baecb2cf522cf132d1a4f8a4205f4b
Bara <biimackra@gmail.com>
2024-10-18 14:04:19 +0700

    Update using Binding & add Library Room

--- app/src/main/java/com/project/uts/LoginActivity.kt
+++ app/src/main/java/com/project/uts/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.project.uts
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
--- app/src/main/java/com/project/uts/MainActivity.kt
+++ app/src/main/java/com/project/uts/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.project.uts
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
